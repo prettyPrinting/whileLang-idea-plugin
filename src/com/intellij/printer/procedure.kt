cd9e1a0ad2974bbd6ef05c6af6dd2f9820a583b3
Igor Ozernykh <Igor.Ozernykh@gmail.com>
2015-07-03 15:05:56 +0300

    added procedures with params

--- src/com/intellij/printer/procedure.kt
+++ src/com/intellij/printer/procedure.kt
@@ -17,6 +17,8 @@ public class ProcedureComponent(
 
     final val PROCNAME_TAG: String
         get() = "procname"
+    final val PARAMLIST_TAG: String
+        get() = "paramlist"
     final val STMTLIST_TAG: String
         get() = "stmtlist"
     
@@ -57,6 +59,45 @@ public class ProcedureComponent(
         val procName = p.getId()
         if (procName == null) { return printer.getEmptySet() }
         return printer.getVariants(procName, context)
+    }
+    
+    private fun addParamListToInsertPlaceMap(
+            p: PsiProcedure
+            , insertPlaceMap: MutableMap<String, SmartInsertPlace>
+            , delta: Int
+    ): Boolean {
+        val paramList = p.getParamList()
+        val paramListTextRange = paramList?.getTextRange()
+        if (paramListTextRange == null) { return false }
+        
+        val text = p.getContainingFile()?.getText()
+        if (text == null) { return false }
+        val fillConstant = text.getFillConstant(paramListTextRange)
+        
+        insertPlaceMap.put(
+               PARAMLIST_TAG
+               , SmartInsertPlace(paramListTextRange.shiftRight(delta), fillConstant, paramList!!.toBox())
+            )
+        return true
+    }
+    
+    private fun prepareParamListVariants(
+            p: PsiProcedure
+            , variants: MutableMap<String, FormatSet>
+            , context: VariantConstructionContext
+    ) {
+        val paramListVariants = getParamListVariants(p, context)
+        if (paramListVariants.isEmpty()) { return }
+        variants.put(PARAMLIST_TAG, paramListVariants)
+    }
+    
+    private fun getParamListVariants(
+            p: PsiProcedure
+            , context: VariantConstructionContext
+    ): FormatSet {
+        val paramList = p.getParamList()
+        if (paramList == null) { return printer.getEmptySet() }
+        return printer.getVariants(paramList, context)
     }
     
     private fun addStmtListToInsertPlaceMap(
@@ -127,6 +168,7 @@ public class ProcedureComponent(
         val variants = HashMap<String, FormatSet>()
     
         prepareProcNameVariants(p, variants, context)
+        prepareParamListVariants(p, variants, context)
         prepareStmtListVariants(p, variants, context)
         
         
@@ -138,6 +180,7 @@ public class ProcedureComponent(
         val set = HashSet<String>()
     
         if (p.getId() != null) { set.add(PROCNAME_TAG) }
+        if (p.getParamList() != null) { set.add(PARAMLIST_TAG) }
         if (p.getStmtList() != null) { set.add(STMTLIST_TAG) }
         
         
@@ -159,6 +202,7 @@ public class ProcedureComponent(
         val text = newP.getText() ?: ""
     
         if (!addProcNameToInsertPlaceMap(newP, insertPlaceMap, negShift)) { return null }
+        addParamListToInsertPlaceMap(newP, insertPlaceMap, negShift)
         if (!addStmtListToInsertPlaceMap(newP, insertPlaceMap, negShift)) { return null }
         
         
